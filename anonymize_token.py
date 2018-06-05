74d8799281d39f309d5d28871915b30689b17f93
Ferry Boender <ferry.boender@gmail.com>
2018-06-05 10:08:35 +0200

    Read token from interactive stdin, to prevent it from being added to the shell history

--- anonymize_token.py
+++ anonymize_token.py
@@ -25,15 +25,27 @@ def get_anon_char(c, char_class_mapping, mask=False):
         return random.choice(char_class_mapping)
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description="Anonymize a secret token (read from stdin)")
-    parser.add_argument("-m", action="store_true", dest="mask", default=False, help="Mask instead of randomize")
+    parser = argparse.ArgumentParser(
+        description="Anonymize a secret token (read from stdin)"
+    )
+    parser.add_argument(
+        "-m",
+        action="store_true",
+        dest="mask",
+        default=False,
+        help="Mask instead of randomize"
+    )
     args = parser.parse_args()
 
-    input = sys.stdin.read().strip()
+    sys.stdout.write("Token: ")
+    if sys.version_info.major > 2:
+        in_token = input()
+    else:
+        in_token = raw_input()
 
     # Go through each char in input and map it to a random matching char.
     result = ""
-    for char in input:
+    for char in in_token:
         for char_class_mapping in char_class_mappings:
             if char in char_class_mapping:
                 result += get_anon_char(char, char_class_mapping, args.mask)
@@ -42,5 +54,5 @@ if __name__ == "__main__":
             # No match found. Just use original char
             result += char
 
-    assert(len(input) == len(result))
+    assert(len(in_token) == len(result))
     print(result)
